0e7ef22e4dd8daec00eaf942ed8d7047799daa36
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-14 00:10:22 +0300

    Refactor page layout and add buttons demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import React from "react"
 
-import { Card } from "@/components/ui/card"
+import { ButtonsDemo } from "@/components/demos/buttons-demo"
 import { CalendarDemo } from "@/components/demos/calendar-demo"
 import { CookieSettingsDemo } from "@/components/demos/cookie-settings-demo"
 import { TeamMembersDemo } from "@/components/demos/team-members-demo"
 
 export default function HomePage() {
   return (
-    <div className="container pt-8 flex flex-row flex-wrap gap-6">
+    <div className="container py-8 flex flex-row flex-wrap gap-6">
       <TeamMembersDemo />
-
       <CalendarDemo />
-
       <CookieSettingsDemo />
+      <ButtonsDemo />
     </div>
   )
 }
