a9461ab6b3cdd4b7b05b0110bcfade5dfbfa511c
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-14 18:47:47 +0300

    Fix spacing issue in ThemeModifiers component

--- features/theme-settings/components/theme-modifiers.tsx
+++ features/theme-settings/components/theme-modifiers.tsx
@@ -10,6 +10,7 @@ import { CssVarColorPicker } from "./css-var-color-picker"
 export function ThemeModifiers() {
   const { resolvedTheme } = useTheme()
   const theme = resolvedTheme === "dark" ? "dark" : "light"
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-1 gap-y-2">
       {Object.keys(colors.light).map((currentKey) => (
