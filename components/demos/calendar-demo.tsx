d57626f412061f039309fcf166cf1a4d5376c74b
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-16 01:06:30 +0300

    Refactor accordion and calendar demos

--- components/demos/calendar-demo.tsx
+++ components/demos/calendar-demo.tsx
@@ -1,18 +1,11 @@
 "use client"
 
 import * as React from "react"
-import Image from "next/image"
 
 import { Calendar } from "@/components/ui/calendar"
 
 import { Section } from "../section"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../ui/card"
+import { Card } from "../ui/card"
 
 export function CalendarDemo() {
   const [date, setDate] = React.useState<Date | undefined>()
