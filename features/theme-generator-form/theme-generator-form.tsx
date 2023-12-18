48579ce284c942b8cf860d048617d9996ef924f1
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-19 01:05:47 +0300

    Refactor theme generator form layout and styling

--- features/theme-generator-form/theme-generator-form.tsx
+++ features/theme-generator-form/theme-generator-form.tsx
@@ -31,65 +31,79 @@ export function ThemeGeneratorForm() {
   }, [color, setColors, backgroundStyle])
 
   return (
-    <div className="mt-8 mb-16 flex flex-wrap gap-8 items-end">
-      <div className="flex flex-col w-fit gap-2">
-        <HexColorPicker
-          color={color}
-          onChange={(value) => {
-            setColor(value)
-            setColorInput(value)
-          }}
-        />
-        <Input
-          placeholder="Color"
-          value={colorInput}
-          onChange={(e) => {
-            setColorInput(e.target.value)
-            if (isValidColor(e.target.value))
-              setColor(Color(e.target.value).hex())
-          }}
-        />
-      </div>
+    <div className="mt-8 mb-16 flex flex-wrap gap-8 items-end justify-between">
+      <div className="flex items-end flex-wrap gap-8">
+        <div className="flex flex-col w-fit gap-2">
+          <HexColorPicker
+            color={color}
+            onChange={(value) => {
+              setColor(value)
+              setColorInput(value)
+            }}
+          />
+          <Input
+            placeholder="Color"
+            value={colorInput}
+            onChange={(e) => {
+              setColorInput(e.target.value)
+              if (isValidColor(e.target.value))
+                setColor(Color(e.target.value).hex())
+            }}
+          />
+        </div>
 
-      <div className="w-fit">
-        <div className="font-semibold mb-2">Dark mode background style</div>
-        <div className="flex flex-wrap gap-2">
-          <Button
-            variant={backgroundStyle === "black" ? "default" : "outline"}
-            onClick={() => setBackgroundStyle("black")}
-          >
-            Black
-          </Button>
+        <div className="w-fit">
+          <div className="font-semibold mb-2">Dark mode background style</div>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-wrap gap-2">
+              <Button
+                className="flex-1"
+                variant={backgroundStyle === "black" ? "default" : "outline"}
+                onClick={() => setBackgroundStyle("black")}
+              >
+                Black
+              </Button>
 
-          <Button
-            variant={backgroundStyle === "gray" ? "default" : "outline"}
-            onClick={() => setBackgroundStyle("gray")}
-          >
-            Gray
-          </Button>
+              <Button
+                className="flex-1"
+                variant={backgroundStyle === "gray" ? "default" : "outline"}
+                onClick={() => setBackgroundStyle("gray")}
+              >
+                Gray
+              </Button>
 
-          <Button
-            variant={backgroundStyle === "grayish" ? "default" : "outline"}
-            onClick={() => setBackgroundStyle("grayish")}
-          >
-            Grayish
-          </Button>
+              <Button
+                className="flex-1"
+                variant={backgroundStyle === "grayish" ? "default" : "outline"}
+                onClick={() => setBackgroundStyle("grayish")}
+              >
+                Grayish
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2">
+              <Button
+                className="flex-1"
+                variant={
+                  backgroundStyle === "slightly-saturated"
+                    ? "default"
+                    : "outline"
+                }
+                onClick={() => setBackgroundStyle("slightly-saturated")}
+              >
+                Slightly Saturated
+              </Button>
 
-          <Button
-            variant={
-              backgroundStyle === "slightly-saturated" ? "default" : "outline"
-            }
-            onClick={() => setBackgroundStyle("slightly-saturated")}
-          >
-            Slightly Saturated
-          </Button>
-
-          <Button
-            variant={backgroundStyle === "saturated" ? "default" : "outline"}
-            onClick={() => setBackgroundStyle("saturated")}
-          >
-            Saturated
-          </Button>
+              <Button
+                className="flex-1"
+                variant={
+                  backgroundStyle === "saturated" ? "default" : "outline"
+                }
+                onClick={() => setBackgroundStyle("saturated")}
+              >
+                Saturated
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
 
