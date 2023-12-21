2fd1e98ff1f15f5d559ffbdd3756ae74d34ff256
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-22 00:22:34 +0300

    Refactor theme generation to support light and dark themes

--- utils/theme-generator/index.ts
+++ utils/theme-generator/index.ts
@@ -1,7 +1,12 @@
 import { ThemeGeneratorParams } from "@/types/theme-generator"
+import { convertAllHexToCssVar } from "@/lib/utils"
 
-import { generateMonochromaticTheme } from "./monochromatic"
+import { generateDarkTheme } from "./dark-theme"
+import { generateLightTheme } from "./light-theme"
 
 export function generateTheme(params: ThemeGeneratorParams) {
-  return generateMonochromaticTheme(params)
+  return {
+    light: convertAllHexToCssVar(generateLightTheme(params)),
+    dark: convertAllHexToCssVar(generateDarkTheme(params)),
+  }
 }
---
+++ utils/theme-generator/light-theme.ts
@@ -0,0 +1,78 @@
+import Color from "color"
+
+import { ThemeGeneratorParams } from "@/types/theme-generator"
+import { ColorVariables } from "@/constants/colors"
+import { convertCssVarToHex } from "@/lib/utils"
+
+function optimizePrimaryColor(color: Color) {
+  const saturation = color.saturationl()
+  const lightness = color.lightness()
+
+  return color
+    .saturationl(50 + (saturation - 50) / 2)
+    .lightness(50 + (lightness - 50) / 2)
+}
+
+export function generateLightTheme({
+  primaryColor: primaryColorString,
+}: ThemeGeneratorParams): ColorVariables {
+  const primary = optimizePrimaryColor(Color(primaryColorString))
+  const primaryForeground = primary.isDark()
+    ? Color("#ffffff")
+    : primary.darken(0.9)
+  const background = primary.saturationl(10).lightness(100)
+  const foreground = background.lightness(10)
+  const card = background
+    .saturationl(background.saturationl() + 5)
+    .lightness(background.lightness() - 2.5)
+  const cardForeground = foreground
+  const secondary = card
+    .saturationl(card.saturationl() + 15)
+    .lightness(card.lightness() - 7.5)
+  const secondaryForeground = foreground
+  const muted = card
+    .saturationl(card.saturationl() + 5)
+    .lightness(card.lightness() - 2.5)
+  const mutedForeground = foreground.lightness(40)
+  const accent = card
+    .saturationl(card.saturationl() + 15)
+    .lightness(card.lightness() - 7.5)
+  const accentForeground = foreground
+  // const destructive = primary.hue(0)
+  // const destructiveForeground = destructive.isDark()
+  //   ? Color("#ffffff")
+  //   : destructive.darken(0.8)
+  const border = card
+    .saturationl(card.saturationl() + 5)
+    .lightness(card.lightness() - 2.5)
+  const input = card
+    .saturationl(card.saturationl() + 10)
+    .lightness(card.lightness() - 5)
+  const ring = input
+    .saturationl(input.saturationl() + 10)
+    .lightness(input.lightness() - 5)
+
+  return {
+    "--background": background.hex(),
+    "--foreground": foreground.hex(),
+    "--card": card.hex(),
+    "--card-foreground": cardForeground.hex(),
+    "--popover": card.hex(),
+    "--popover-foreground": cardForeground.hex(),
+    "--primary": primary.hex(),
+    "--primary-foreground": primaryForeground.hex(),
+    "--secondary": secondary.hex(),
+    "--secondary-foreground": secondaryForeground.hex(),
+    "--muted": muted.hex(),
+    "--muted-foreground": mutedForeground.hex(),
+    "--accent": accent.hex(),
+    "--accent-foreground": accentForeground.hex(),
+    // "--destructive": destructive.hex(),
+    // "--destructive-foreground": destructiveForeground.hex(),
+    "--destructive": convertCssVarToHex("0 84.2% 60.2%"),
+    "--destructive-foreground": convertCssVarToHex("0 0% 98%"),
+    "--border": border.hex(),
+    "--input": input.hex(),
+    "--ring": ring.hex(),
+  }
+}
