8518f3cb25d6b60f2bff73c70e774d4545cdf467
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-22 14:55:40 +0300

    Refactor color picker component and theme generator form

--- features/theme-generator-form/components/color-picker.tsx
+++ features/theme-generator-form/components/color-picker.tsx
@@ -2,7 +2,6 @@
 
 import React from "react"
 import Color from "color"
-import { HexColorPicker } from "react-colorful"
 import { useLocalStorage } from "react-use"
 
 import { isValidColor } from "@/lib/utils"
@@ -15,20 +14,29 @@ interface Props {
 
 export default function ColorPicker({ value, onChange }: Props) {
   const [colorInput, setColorInput] = useLocalStorage(
-    "PRIMARY_COLOR",
-    "#3b82f6"
+    "PRIMARY_COLOR_INPUT",
+    value || "#3b82f6"
   )
 
   return (
-    <div className="flex flex-col gap-2">
-      <HexColorPicker
-        color={value}
-        onChange={(value) => {
-          onChange(value)
-          setColorInput(value)
-        }}
-      />
+    <div className="relative w-full flex items-center">
+      <label
+        className="absolute block w-7 h-7 left-4 rounded-full cursor-pointer overflow-hidden"
+        style={{ backgroundColor: value }}
+      >
+        <input
+          type="color"
+          className="opacity-0 cursor-pointer"
+          value={value}
+          onChange={(e) => {
+            onChange(e.target.value)
+            setColorInput(e.target.value)
+          }}
+        />
+      </label>
+
       <Input
+        className="bg-background w-full h-14 pl-14 rounded-full"
         placeholder="Color"
         value={colorInput}
         onChange={(e) => {
@@ -38,5 +46,23 @@ export default function ColorPicker({ value, onChange }: Props) {
         }}
       />
     </div>
+    // <div className="flex flex-col gap-2">
+    //   <HexColorPicker
+    //     color={value}
+    //     onChange={(value) => {
+    //       onChange(value)
+    //       setColorInput(value)
+    //     }}
+    //   />
+    //   <Input
+    //     placeholder="Color"
+    //     value={colorInput}
+    //     onChange={(e) => {
+    //       setColorInput(e.target.value)
+    //       if (isValidColor(e.target.value))
+    //         onChange(Color(e.target.value).hex())
+    //     }}
+    //   />
+    // </div>
   )
 }
--- features/theme-generator-form/theme-generator-form.tsx
+++ features/theme-generator-form/theme-generator-form.tsx
@@ -3,16 +3,8 @@
 import React from "react"
 
 import { useThemeStore } from "@/store/theme-store"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 import ColorPicker from "./components/color-picker"
-import CopyButton from "./components/copy-button"
 import { DarkBgStyleSelector } from "./components/dark-bg-style-selector"
 
 export function ThemeGeneratorForm() {
@@ -21,39 +13,24 @@ export function ThemeGeneratorForm() {
   const setFieldStore = useThemeStore((state) => state.setField)
 
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
-          <div className="space-y-1.5">
-            <CardTitle>Theme Settings</CardTitle>
-            <CardDescription>
-              Adjust the theme settings to your liking and generate a theme
-              file.
-            </CardDescription>
-          </div>
-          <div>
-            <CopyButton />
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-col gap-8 items-center sm:flex-row sm:flex-wrap sm:items-end">
-          <div className="w-fit mx-auto sm:mx-[unset]">
-            <ColorPicker
-              value={color!}
-              onChange={(v) => setFieldStore("color", v)}
-            />
-          </div>
+    <div className="max-w-md w-full">
+      <ColorPicker value={color!} onChange={(v) => setFieldStore("color", v)} />
+    </div>
+    // <div className="flex flex-col gap-8 items-center sm:flex-row sm:flex-wrap sm:items-end">
+    //   <div className="w-fit mx-auto sm:mx-[unset]">
+    //     <ColorPicker
+    //       value={color!}
+    //       onChange={(v) => setFieldStore("color", v)}
+    //     />
+    //   </div>
 
-          <div className="max-w-xs sm:w-fit">
-            <div className="font-semibold mb-2">Dark mode background style</div>
-            <DarkBgStyleSelector
-              value={darkModeBgStyle}
-              onValueChange={(val) => setFieldStore("darkModeBgStyle", val)}
-            />
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+    //   <div className="max-w-xs sm:w-fit">
+    //     <div className="font-semibold mb-2">Dark mode background style</div>
+    //     <DarkBgStyleSelector
+    //       value={darkModeBgStyle}
+    //       onValueChange={(val) => setFieldStore("darkModeBgStyle", val)}
+    //     />
+    //   </div>
+    // </div>
   )
 }
