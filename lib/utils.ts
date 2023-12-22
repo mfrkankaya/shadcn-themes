3d8b476f3e7fb3c8eaf6c83ed2b38ecdba8bfa47
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-22 20:27:38 +0300

    Update CSS variables for light and dark themes

--- lib/utils.ts
+++ lib/utils.ts
@@ -55,43 +55,60 @@ export function getCopyableCssVariables({
   :root {
     --background: ${light["--background"]};
     --foreground: ${light["--foreground"]};
+
     --card: ${light["--card"]};
     --card-foreground: ${light["--card-foreground"]};
+
     --popover: ${light["--popover"]};
     --popover-foreground: ${light["--popover-foreground"]};
+
     --primary: ${light["--primary"]};
     --primary-foreground: ${light["--primary-foreground"]};
+
     --secondary: ${light["--secondary"]};
     --secondary-foreground: ${light["--secondary-foreground"]};
+
     --muted: ${light["--muted"]};
     --muted-foreground: ${light["--muted-foreground"]};
+
     --accent: ${light["--accent"]};
     --accent-foreground: ${light["--accent-foreground"]};
+
     --destructive: ${light["--destructive"]};
     --destructive-foreground: ${light["--destructive-foreground"]};
+
     --border: ${light["--border"]};
     --input: ${light["--input"]};
     --ring: ${light["--ring"]};
+
     --radius: 0.5rem;
   }
   
   .dark {
     --background: ${dark["--background"]};
     --foreground: ${dark["--foreground"]};
+
     --card: ${dark["--card"]};
     --card-foreground: ${dark["--card-foreground"]};
+
     --popover: ${dark["--popover"]};
     --popover-foreground: ${dark["--popover-foreground"]};
+
     --primary: ${dark["--primary"]};
     --primary-foreground: ${dark["--primary-foreground"]};
+
     --secondary: ${dark["--secondary"]};
     --secondary-foreground: ${dark["--secondary-foreground"]};
+
     --muted: ${dark["--muted"]};
     --muted-foreground: ${dark["--muted-foreground"]};
+
     --accent: ${dark["--accent"]};
     --accent-foreground: ${dark["--accent-foreground"]};
+
     --destructive: ${dark["--destructive"]};
     --destructive-foreground: ${dark["--destructive-foreground"]};
+
     --border: ${dark["--border"]};
     --input: ${dark["--input"]};
     --ring: ${dark["--ring"]};
