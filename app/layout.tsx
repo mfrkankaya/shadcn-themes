2842313c9897258f6c79825e6077a71a9338d824
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-24 14:52:25 +0300

    add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 
 import type { Metadata } from "next"
+import Script from "next/script"
 import { GeistSans } from "geist/font/sans"
 
 import { cn } from "@/lib/utils"
@@ -12,7 +13,8 @@ import { ThemeSync } from "@/components/theme-sync"
 
 export const metadata: Metadata = {
   title: "Generate Theme - Shadcn/UI Theme Generator",
-  description: "Theme generator for shadcn/ui components. Generate a theme for your app by selecting a color and copy-pasting the generated css variables.",
+  description:
+    "Theme generator for shadcn/ui components. Generate a theme for your app by selecting a color and copy-pasting the generated css variables.",
 }
 
 export default function RootLayout({
@@ -22,7 +24,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn(GeistSans.variable)} suppressHydrationWarning>
+      <head>
+        <Script id="google-tag-manager">
+          {`<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRCZHWB4');</script>
+<!-- End Google Tag Manager -->`}
+        </Script>
+      </head>
       <body>
+        <Script id="google-tag-manager-noscript">
+          {`<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRCZHWB4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`}
+        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
