d302ecec09a39d7480e92e017e23400f229bbff9
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-24 17:52:00 +0300

    Remove Google Tag Manager and add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css"
 
 import type { Metadata } from "next"
-import Script from "next/script"
 import { GeistSans } from "geist/font/sans"
 
 import { cn } from "@/lib/utils"
@@ -24,24 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn(GeistSans.variable)} suppressHydrationWarning>
-      <head>
-        <Script id="google-tag-manager">
-          {`<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WRCZHWB4');</script>
-<!-- End Google Tag Manager -->`}
-        </Script>
-      </head>
       <body>
-        <Script id="google-tag-manager-noscript">
-          {`<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRCZHWB4"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->`}
-        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
