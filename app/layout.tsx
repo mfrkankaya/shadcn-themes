a0a9d5980a526ae807d93df209b0c72f7be03b2e
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-24 21:21:09 +0300

    Add SpeedInsights component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GeistSans } from "geist/font/sans"
 
 import { cn } from "@/lib/utils"
@@ -38,6 +39,7 @@ export default function RootLayout({
           <Toaster />
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
