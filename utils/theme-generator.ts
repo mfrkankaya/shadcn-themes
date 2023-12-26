18e65fb5dd19802fd707841111a71dbb1aded3d5
Furkan Kaya <mfrkankaya@gmail.com>
2023-12-26 23:35:35 +0300

    Refactor theme generator saturation and lightness values

--- utils/theme-generator.ts
+++ utils/theme-generator.ts
@@ -6,35 +6,35 @@ import { convertAllHexToCssVar, convertCssVarToHex } from "@/lib/utils"
 function generateLightTheme({ color }: { color: string }): ColorVariables {
   const primary = Color(color)
   const primaryForeground = primary.isDark()
-    ? primary.saturationl(5).lightness(98)
-    : primary.saturationl(5).lightness(2)
+    ? primary.saturationl(2.5).lightness(98)
+    : primary.saturationl(2.5).lightness(2)
   const background = primary.saturationl(0).lightness(100)
   const foreground = background
-    .saturationl(background.saturationl() + 5)
-    .lightness(10)
+    .saturationl(background.saturationl() + 2.5)
+    .lightness(5)
   const card = background
   const cardForeground = foreground
   const secondary = card
-    .saturationl(card.saturationl() + 20)
-    .lightness(card.lightness() - 10)
+    .saturationl(card.saturationl() + 15)
+    .lightness(card.lightness() - 7.5)
   const secondaryForeground = foreground
   const muted = card
-    .saturationl(card.saturationl() + 7.5)
-    .lightness(card.lightness() - 5)
+    .saturationl(card.saturationl() + 5)
+    .lightness(card.lightness() - 3)
   const mutedForeground = foreground.lightness(40)
   const accent = card
-    .saturationl(card.saturationl() + 10)
-    .lightness(card.lightness() - 5)
+    .saturationl(card.saturationl() + 7.5)
+    .lightness(card.lightness() - 3)
   const accentForeground = foreground
   const border = card
     .saturationl(card.saturationl() + 10)
-    .lightness(card.lightness() - 5)
-  const input = card
-    .saturationl(card.saturationl() + 15)
     .lightness(card.lightness() - 7.5)
+  const input = border
+    .saturationl(border.saturationl() + 5)
+    .lightness(border.lightness() - 5)
   const ring = input
-    .saturationl(input.saturationl() + 15)
-    .lightness(input.lightness() - 7.5)
+    .saturationl(input.saturationl() + 30)
+    .lightness(input.lightness() - 15)
 
   return {
     "--background": background.hex(),
@@ -62,8 +62,8 @@ function generateLightTheme({ color }: { color: string }): ColorVariables {
 function generateDarkTheme({ color }: { color: string }): ColorVariables {
   const primary = Color(color)
   const primaryForeground = primary.isDark()
-    ? primary.saturationl(5).lightness(98)
-    : primary.saturationl(5).lightness(2)
+    ? primary.saturationl(2.5).lightness(98)
+    : primary.saturationl(2.5).lightness(2)
   const background = primary.saturationl(7.5).lightness(5)
   const foreground = background
     .saturationl(background.saturationl() + 5)
@@ -85,14 +85,14 @@ function generateDarkTheme({ color }: { color: string }): ColorVariables {
     .lightness(card.lightness() + 5)
   const accentForeground = foreground
   const border = card
-    .saturationl(card.saturationl() + 10)
-    .lightness(card.lightness() + 5)
-  const input = card
-    .saturationl(card.saturationl() + 15)
-    .lightness(card.lightness() + 7.5)
+    .saturationl(card.saturationl() + 6)
+    .lightness(card.lightness() + 3)
+  const input = border
+    .saturationl(border.saturationl() + 3)
+    .lightness(border.lightness() + 3)
   const ring = input
-    .saturationl(input.saturationl() + 15)
-    .lightness(input.lightness() + 7.5)
+    .saturationl(input.saturationl() + 30)
+    .lightness(input.lightness() + 15)
 
   return {
     "--background": background.hex(),
