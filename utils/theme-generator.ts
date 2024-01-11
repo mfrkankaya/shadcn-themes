f15999ce0c7cd7214d7d2355001d7b23df038559
Furkan Kaya <mfrkankaya@gmail.com>
2024-01-11 18:48:04 +0300

    refactor: color generation logic

--- utils/theme-generator.ts
+++ utils/theme-generator.ts
@@ -24,102 +24,81 @@ class LightPalette {
     this.options = options
   }
 
+  private getSaturation() {
+    const bgStyle = this.options.backgroundStyle
+    if (isWOBG(Color(this.options.color))) return 0
+    if (bgStyle === "white") return 0
+    if (bgStyle === "grayish") return 6
+    if (bgStyle === "slightly-saturated") return 9
+    return 6
+  }
+
+  private getLightness() {
+    const bgStyle = this.options.backgroundStyle
+    if (isWOBG(Color(this.options.color))) return 100
+    if (bgStyle === "white") return 100
+    if (bgStyle === "grayish") return 98
+    if (bgStyle === "slightly-saturated") return 95
+    return 100
+  }
+
   getPrimaryForeground() {
-    return this.primary.isDark()
-      ? this.primary
-          .saturationl(this.isWhiteOrBlackOrGray ? 0 : 2.5)
-          .lightness(98)
-      : this.primary
-          .saturationl(this.isWhiteOrBlackOrGray ? 0 : 2.5)
-          .lightness(2)
+    return this.primary
+      .saturationl(this.getSaturation())
+      .lightness(this.primary.isDark() ? 98 : 2)
   }
 
   getBackground() {
-    const bgStyle = this.options.backgroundStyle
-    if (bgStyle === "white") return this.primary.saturationl(0).lightness(100)
-    if (bgStyle === "grayish") return this.primary.saturationl(5).lightness(98)
-    // if (bgStyle === "slightly-saturated")
-    return this.primary.saturationl(10).lightness(97)
+    return this.getPrimaryForeground().lightness(this.getLightness())
   }
 
   getForeground() {
-    const background = this.getBackground()
-    return background
-      .saturationl(
-        this.isWhiteOrBlackOrGray ? 0 : background.saturationl() + 2.5
-      )
-      .lightness(5)
+    return this.getBackground().lightness(5)
   }
 
   getCard() {
-    const background = this.getBackground()
-    const isSameBg = this.options.isCardsAndBackgroundSameColor
-    if (isSameBg) return background
-    return background
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : background.saturationl() + 1)
-      .lightness(background.lightness() - 1)
+    if (this.options.isCardsAndBackgroundSameColor) return this.getBackground()
+    return this.getBackground().darken(0.007)
   }
 
   getCardForeground() {
-    const foreground = this.getForeground()
-    return foreground
+    return this.getForeground()
   }
 
   getSecondary() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 15)
-      .lightness(card.lightness() - 7.5)
+    return this.getCard().saturate(0.2).darken(0.04)
   }
 
   getSecondaryForeground() {
-    const foreground = this.getForeground()
-    return foreground
+    return this.getForeground()
   }
 
   getMuted() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 5)
-      .lightness(card.lightness() - 3)
+    return this.getCard().darken(0.04)
   }
 
   getMutedForeground() {
-    const foreground = this.getForeground()
-    return foreground.lightness(40)
+    return this.getForeground().lighten(8)
   }
 
   getAccent() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 7.5)
-      .lightness(card.lightness() - 3)
+    return this.getCard().saturate(0.08).darken(0.04)
   }
 
   getAccentForeground() {
-    const foreground = this.getForeground()
-    return foreground
+    return this.getForeground()
   }
 
   getBorder() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 10)
-      .lightness(card.lightness() - 7.5)
+    return this.getCard().darken(0.08)
   }
 
   getInput() {
-    const border = this.getBorder()
-    return border
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : border.saturationl() + 5)
-      .lightness(border.lightness() - 5)
+    return this.getBorder().darken(0.08)
   }
 
   getRing() {
-    const input = this.getInput()
-    return input
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : input.saturationl() + 30)
-      .lightness(input.lightness() - 15)
+    return this.getInput().saturate(3).darken(0.1)
   }
 }
 
@@ -175,105 +154,92 @@ class DarkPalette {
     this.options = options
   }
 
+  private getSaturation() {
+    const bgStyle = this.options.backgroundStyle
+    if (isWOBG(Color(this.options.color))) return 0
+    if (bgStyle === "black") return 0
+    if (bgStyle === "gray") return 2
+    if (bgStyle === "grayish") return 5
+    if (bgStyle === "slightly-saturated") return 8
+    return 0
+  }
+
+  private getLightness() {
+    const bgStyle = this.options.backgroundStyle
+    if (bgStyle === "black") return 0
+    if (bgStyle === "gray") return 5
+    if (bgStyle === "grayish") return 6
+    if (bgStyle === "slightly-saturated") return 8
+    return 5
+  }
+
   getPrimaryForeground() {
-    return this.primary.isDark()
-      ? this.primary
-          .saturationl(this.isWhiteOrBlackOrGray ? 0 : 2.5)
-          .lightness(98)
-      : this.primary
-          .saturationl(this.isWhiteOrBlackOrGray ? 0 : 2.5)
-          .lightness(2)
+    return this.primary
+      .saturationl(this.getSaturation())
+      .lightness(this.primary.isDark() ? 98 : 2)
   }
 
   getBackground() {
-    const bgStyle = this.options.backgroundStyle
-    if (bgStyle === "black") return this.primary.saturationl(0).lightness(0)
-    if (bgStyle === "gray") return this.primary.saturationl(0).lightness(5)
-    if (bgStyle === "grayish")
-      return this.primary
-        .saturationl(this.isWhiteOrBlackOrGray ? 0 : 10)
-        .lightness(5)
-    // if (bgStyle === "slightly-saturated")
     return this.primary
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : 10)
-      .lightness(7)
+      .saturationl(this.getSaturation())
+      .lightness(this.getLightness())
   }
 
   getForeground() {
-    const background = this.getBackground()
-    return background
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : background.saturationl() + 5)
-      .lightness(90)
+    return this.getBackground().lightness(98)
   }
 
   getCard() {
     const background = this.getBackground()
     const isSameBg = this.options.isCardsAndBackgroundSameColor
     if (isSameBg) return background
-    return background.lightness(
-      background.lightness() +
-        (this.options.backgroundStyle === "black" ? 4 : 2)
-    )
+    return background
+      .lightness(
+        this.options.backgroundStyle === "black" ? 3 : background.lightness()
+      )
+      .lighten(this.options.backgroundStyle === "black" ? 0 : 0.2)
   }
 
   getCardForeground() {
-    const foreground = this.getForeground()
-    return foreground
+    return this.getForeground()
   }
 
   getSecondary() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 15)
-      .lightness(card.lightness() + 7)
+    return this.getCard().saturate(5).lighten(1)
   }
 
   getSecondaryForeground() {
-    const foreground = this.getForeground()
-    return foreground
+    return this.getForeground()
   }
 
   getMuted() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 7.5)
-      .lightness(card.lightness() + 5)
+    return this.getCard().lighten(2)
   }
 
   getMutedForeground() {
-    const foreground = this.getForeground()
-    return foreground
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : 7.5)
-      .lightness(60)
+    return this.getForeground().darken(0.5)
   }
 
   getAccent() {
-    const card = this.getCard()
-    return card
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : card.saturationl() + 10)
-      .lightness(card.lightness() + 5)
+    return this.getCard().saturate(8).lighten(1)
   }
 
   getAccentForeground() {
-    const foreground = this.getForeground()
-    return foreground
+    return this.getForeground()
   }
 
   getBorder() {
-    const card = this.getCard()
-    return card.lightness(card.lightness() + 8)
+    return this.getCard().lightness(
+      Math.min(12, this.getCard().lightness() + 8)
+    )
   }
 
   getInput() {
-    const border = this.getBorder()
-    return border.lightness(border.lightness() + 8)
+    return this.getBorder().lighten(0.5)
   }
 
   getRing() {
-    const input = this.getInput()
-    return input
-      .saturationl(this.isWhiteOrBlackOrGray ? 0 : input.saturationl() + 40)
-      .lightness(input.lightness() + 20)
+    return this.primary.lightness(40)
   }
 }
 
